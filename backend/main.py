8c2f83cc601cebde1c9ec48ebc7065fa8ab11494
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-01-15 09:41:50 +0100

    Runs app, supports health route

--- backend/main.py
+++ backend/main.py
@@ -13,16 +13,52 @@ import os
 import time
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from backend.models.models import CustomResponse
-from backend.routers import operations
-
-app = FastAPI(
-    title="VintedBot",
-    description="Auto-manage purchases and sales from Vinted",
-    version="1.0",
-    license="Guys Copyright"
-)
+from backend.routers import health
 
 
+if __name__ == "__main__":
 
+    # Get arguments
+    parser = argparse.ArgumentParser(description="VintedBot")
+    parser.add_argument(
+        "-p",
+        "--port",
+        action="store",
+        default=8000,
+        help="Specify port to use",
+        required=False
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        action="store",
+        default="vinted_bot.log",
+        help="Specify output log file",
+        required=False
+    )
+
+    args = parser.parse_args()
+
+    # Set timezone to UTC
+    os.environ["TZ"] = "UTC"
+    time.tzset()
+
+    # Define logs config
+    logging.basicConfig(
+        filename=args.log,
+        level=logging.DEBUG,
+        format="%(asctime)s -- %(funcName)s -- %(levelname)s -- %(message)s"
+    )
+
+    # Define app, include routes
+    app = FastAPI(
+        title="VintedBot",
+        description="Auto-manage purchases and sales from Vinted",
+        version="1.0",
+        license="Guys Copyright"
+    )
+
+    app.include_router(health.router)
+
+    # Run app - override log_config parameter
+    uvicorn.run(app, host="127.0.0.1", port=int(args.port), log_config=None)
