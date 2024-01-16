bfa7965659718d3964ed075ad20264e8ee64b024
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-01-16 22:30:50 +0100

    Added global config to get clothes

--- config/defines.py
+++ config/defines.py
@@ -13,3 +13,12 @@
 VINTED_API_URL = f"https://www.vinted.fr/api/v2"
 # End point used to get clothes
 VINTED_PRODUCTS_ENDPOINT = "catalog/items"
+# Number of retries to get clothes
+NB_RETRIES = 5
+# Headers to pass for get_clothes route
+HEADERS_GET_CLOTHES = {
+    "User-Agent": "PostmanRuntime/7.28.4",
+    "Host": "www.vinted.fr"
+}
+# Vinted AUTH URL to get clothes
+VINTED_AUTH_URL = "https://www.vinted.fr/auth/token_refresh"
