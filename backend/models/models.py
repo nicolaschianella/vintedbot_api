f968d84b2ddd32be5e15896aa863d28dafd65857
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-02-04 18:49:31 +0100

    Added model for update_requests route

--- backend/models/models.py
+++ backend/models/models.py
@@ -43,3 +43,36 @@ class InputGetClothes(BaseModel):
     order: str = "newest_first"
     # Not a filter itself but necessary for the API to work
     currency: str = "EUR"
+
+
+class InputUpdateRequests(BaseModel):
+    # Request name
+    name: str = "DEFAULT"
+    # Creation date
+    creation_date: str = ""
+    # Page number to get items from per search
+    page: str = "1"
+    # Number of items for this page to get per search
+    per_page: str = "96"
+    # "Rechercher" field
+    search_text: str = ""
+    # "Catégorie" filter
+    catalog_ids: str = ""
+    # "Couleur" field (if multiple, separate them with a coma)
+    color_ids: str = ""
+    # "Marque" field (if multiple, separate them with a coma)
+    brand_ids: str = ""
+    # "Taille" filter (if multiple, separate them with a coma)
+    size_ids: str = ""
+    # "Matière" field (if multiple, separate them with a coma)
+    material_ids: str = ""
+    # Price from -> parameter only appears in API if value != ""
+    price_from: str = ""
+    # Price to -> parameter only appears in API if value != ""
+    price_to: str = ""
+    # "Etat" field (if multiple, separate them with a coma)
+    status_ids: str = ""
+    # "Trier par" field
+    order: str = "newest_first"
+    # Not a filter itself but necessary for the API to work
+    currency: str = "EUR"
